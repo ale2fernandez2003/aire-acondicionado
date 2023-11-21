f5ba4f84d6b598d6f551768218d4c19ea2b5bfa2
Alejandro <ale2fernandez2003@gmail.com>
2023-11-21 12:30:49 +0100

    incremento maximo y minimo

--- AireAcondicionado.java
+++ AireAcondicionado.java
@@ -9,22 +9,47 @@ public class AireAcondicionado
 {
     // instance variables - replace the example below with your own
     private float temperatura;
+    private float minimo;
+    private float maximo; 
+    private float incremento;
     
-    public AireAcondicionado()
-    {
+    public AireAcondicionado(float minimoDe, float maximoDe){
         temperatura = 15.0f;
+        incremento = 3.0f;
+        minimo = minimoDe;
+        maximo = maximoDe;
     }
 
-    public void subirTemperatura (){
-        temperatura = temperatura + 5;
+    public void subirTemperatura(){
+        if (maximo<temperatura){
+            System.out.println();
+        }
+        else {
+            temperatura = temperatura + incremento;
+        }
     }
     
-    public void bajarTemperatura (){
-        temperatura = temperatura - 5;
+    public void bajarTemperatura(){
+        if (minimo<temperatura){
+            System.out.println();
+        }
+        else {
+            temperatura = temperatura - incremento;
+        }
     }
-
-    public float getTemperatura (){
+    
+    public float getTemperatura(){
         return temperatura;
+    }
+    
+    public float setIncremento(float incrementoDe){
+        if (incremento<0){
+            System.out.println();
+        }
+        else {
+            incremento = incremento + incrementoDe;
+        }
+        return incremento;
     }
     
 }
