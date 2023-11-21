7fb5ba5b58a261cae63814051784e81ef200e065
Alejandro <ale2fernandez2003@gmail.com>
2023-11-21 12:06:53 +0100

    metodos uevos

--- AireAcondicionado.java
+++ AireAcondicionado.java
@@ -8,26 +8,23 @@
 public class AireAcondicionado
 {
     // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class AireAcondicionado
-     */
+    private float temperatura;
+    
     public AireAcondicionado()
     {
-        // initialise instance variables
-        x = 0;
+        temperatura = 15.0f;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public void subirTemperatura (){
+        temperatura = temperatura + 5;
     }
+    
+    public void bajarTemperatura (){
+        temperatura = temperatura - 5;
+    }
+
+    public float getTemperatura (){
+        return temperatura;
+    }
+    
 }
